2035988158a8492dc9a60f13cbdc2214eaa951a1
Jimmy Salazar <jimmyr.salazar@gmail.com>
2024-05-09 13:25:15 -0500

    feat: add custom naming convention

--- src/main/java/com/jimmysalazar/metrics/MetricsApplication.java
+++ src/main/java/com/jimmysalazar/metrics/MetricsApplication.java
@@ -1,9 +1,11 @@
 package com.jimmysalazar.metrics;
 
 import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Meter;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Metrics;
 import io.micrometer.core.instrument.composite.CompositeMeterRegistry;
+import io.micrometer.core.instrument.config.NamingConvention;
 import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
 
 public class MetricsApplication {
@@ -15,6 +17,7 @@ public class MetricsApplication {
 
         //CompositeMeterRegistry compositeMeterRegistry = new CompositeMeterRegistry();
 
+        // tags para segmentar métricas
         Counter counter = compositeMeterRegistry.counter("numero.empleados","oficina","Lima");
         //Counter counter = compositeMeterRegistry.counter("numero.empleados","oficina","Lima");
         // son inútiles sin un meterRegistry
@@ -31,5 +34,27 @@ public class MetricsApplication {
         counter.increment(200);
 
         System.out.printf("Número de empleados %f",counter.count());
+        foo();
+        System.out.printf("Número de empleados %f",counter.count());
     }
+
+    private static void foo() {
+        CompositeMeterRegistry globalRegistry = Metrics.globalRegistry;
+
+        // La convención de nombres se puede modificar con una lambda
+        /*
+        globalRegistry.config().namingConvention(new NamingConvention() {
+            @Override
+            public String name(String s, Meter.Type type, String s1) {
+                return null;
+            }
+        });
+
+         */
+
+        Counter counter = globalRegistry.counter("numero.empleados","oficina","Lima");
+        counter.increment(150);
+    }
+
+
 }
